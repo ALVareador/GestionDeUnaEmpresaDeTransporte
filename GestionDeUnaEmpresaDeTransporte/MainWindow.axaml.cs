0dad862fc0c9d3abb53e505b7493005dd668bef8
Álvaro <95368497+ALVareador@users.noreply.github.com>
2022-11-30 14:06:59 +0100

    Commit Inicial

---
+++ GestionDeUnaEmpresaDeTransporte/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GestionDeUnaEmpresaDeTransporte
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
