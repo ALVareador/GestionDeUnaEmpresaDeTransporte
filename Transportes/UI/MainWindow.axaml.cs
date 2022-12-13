5a2a7975fa9e2696b758b60fae036bb11286d3c5
silvquiroz <118176310+silvquiroz@users.noreply.github.com>
2022-12-13 02:51:32 +0100

    Add files via upload

---
+++ Transportes/UI/MainWindow.axaml.cs
@@ -0,0 +1,126 @@
+using Avalonia.Controls;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using ProyDIA.Core;
+using System.Diagnostics;
+
+namespace ProyDIA.UI;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+
+        var opExit = this.FindControl<MenuItem>("OpExit");
+        var opInsert = this.FindControl<MenuItem>("OpInsert");
+        var btRegistrar = this.FindControl<Button>("BtRegistrar");
+        var dtTransportes = this.FindControl<DataGrid>("DtTransportes");
+        var opDeleteAll = this.FindControl<MenuItem>("OpDeleteAll");
+        opInsert.DoubleTapped += (_, _) => this.OnExit();
+
+
+        Debug.Assert(opExit != null, "opExit not found in XAML");
+        Debug.Assert(opInsert != null, "opInsert not found in XAML");
+        Debug.Assert(opDeleteAll != null, "opDeleteAll not found in XAML");
+        Debug.Assert(btRegistrar != null, "btRegistrar not found in XAML");
+        Debug.Assert(dtTransportes != null, "dtTransportes not found in XAML");
+
+
+        opExit.Click += (_, _) => this.OnExit();
+        opInsert.Click += (_, _) => this.OnAdd();
+        opDeleteAll.Click += (_, _) => this.OnDeleteAll();
+        btRegistrar.Click += (_, _) => this.OnAdd();
+        dtTransportes.SelectionChanged += (_, _) => this.OnTransporteSelected();
+        this.Closed += (_, _) => this.OnClose();
+
+
+        this.RegistroTransportes = XmlRegistroTransportes.RecuperaXml();
+        dtTransportes.Items = this.RegistroTransportes;
+    }
+
+    void InitializeComponent()
+    {
+        this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public void OnExit()
+    {
+        new XmlRegistroTransportes(this.RegistroTransportes).GuardaXml();
+        this.Close();
+    }
+
+    async void OnAdd()
+    {
+        var transporteDlg = new TransporteDlg();
+        await transporteDlg.ShowDialog(this);
+
+        
+        if(!transporteDlg.IsCancelled)
+        {
+            var t = new Transporte(transporteDlg.Matricula,
+                transporteDlg.Tipo, transporteDlg.Cliente,
+                transporteDlg.FechaContra, transporteDlg.Kms,
+                transporteDlg.FechaSal, transporteDlg.FechaEntre);
+            t.SueldoHora = transporteDlg.SueldoHora;
+            t.PrecioLitro = transporteDlg.PrecioLitro;
+            t.CantLtKms = transporteDlg.CantLtKms;
+            t.Update();
+
+            this.RegistroTransportes.Add(t);
+        }
+        
+
+        return;
+    }
+
+    void OnTransporteSelected()
+    {
+        var dtTransportes = this.FindControl<DataGrid>("DtTransportes");
+        
+
+        Debug.Assert(dtTransportes != null, "dtTransportes not found in XAML");
+
+        Transporte? transporte = (Transporte) dtTransportes.SelectedItem;
+        int position = dtTransportes.SelectedIndex;
+        
+        var transporteDlg = new TransporteDlg(transporte);
+        transporteDlg.ShowDialog(this);
+
+        if (!transporteDlg.IsCancelled)
+        {
+            var t = new Transporte(transporteDlg.Matricula,
+                transporteDlg.Tipo, transporteDlg.Cliente,
+                transporteDlg.FechaContra, transporteDlg.Kms,
+                transporteDlg.FechaSal, transporteDlg.FechaEntre);
+            t.SueldoHora = transporteDlg.SueldoHora;
+            t.PrecioLitro = transporteDlg.PrecioLitro;
+            t.CantLtKms = transporteDlg.CantLtKms;
+            t.Update();
+
+            this.RegistroTransportes.Modificar_av(position, t);
+        }
+
+        
+
+    }
+
+    void OnClose()
+    {
+        new XmlRegistroTransportes(this.RegistroTransportes).GuardaXml();
+    }
+
+    void OnDeleteAll()
+    {
+        this.RegistroTransportes.Clear();
+    }
+
+    public RegistroTransportes RegistroTransportes
+    {
+        get;
+    }
+}
